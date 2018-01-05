a6849662427c14b129cd708b566945e73cbabe8a
George Colon <geocolon@gmail.com>
2018-01-05 15:01:52 -0800

    1/5/2018 Updating
    Found the first function largestEl.

--- scripts.js
+++ scripts.js
@@ -1,32 +1,45 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
+  largestEl: function(array){
     // your code here
+    // loop the given array
+    var compareElement = array[0];
+    console.log('This is the compare element #1', compareElement);
+    for (var i = 1; i < array.length; i++){
+     if (array[i] > compareElement){
+       compareElement = array[i];
+     } else if (array[i] === compareElement) {
+       continue;
+     } else {
+       continue;
+     }
+    }
+    return compareElement;
   },
-  
-  reversed: function(){  
+
+  reversed: function(){
     // your code here
   },
 
-  loudSnakeCase: function(){  
+  loudSnakeCase: function(){
     // your code here
   },
 
-  compareArrays: function(){ 
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
