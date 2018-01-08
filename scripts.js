fb0640d256588148cb27ca1ae9010ce728f5d5b2
George Colon <geocolon@gmail.com>
2018-01-08 15:32:47 -0800

    Updated 01/08/2018
    Did all I can do for LoudSnakeCase. It works but the test is failing or "Pending...".

--- scripts.js
+++ scripts.js
@@ -18,12 +18,34 @@ var sprintFunctions = {
     return compareElement;
   },
 
-  reversed: function(){
+  reversed: function(string){
     // your code here
+    var splitString = string.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
   },
 
-  loudSnakeCase: function(){
+  loudSnakeCase: function(string){
     // your code here
+    var reWhiteSpace = new RegExp("/^\s+$/");
+var punctuationless = exampleString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+var normalString = punctuationless.replace(/\s{2,}/g," ");
+function capitalize_Words(str)
+{
+  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+var allCapString = capitalize_Words(normalString);
+var splitString = allCapString.split('');
+var endString = [];
+  for (i = 0; i < splitString.length; i++){
+  if(splitString[i] === ' '){
+       endString.push('_')
+     } else {
+       endString.push(splitString[i]);
+     }
+   }
+   return console.log(endString.join(''));
   },
 
   compareArrays: function(){
